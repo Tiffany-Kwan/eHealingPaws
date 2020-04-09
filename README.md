e08f19bdd2bde3bb0684825effac80e369e463b4
Yiwen Shang <yiwen.shang@ucdconnect.ie>
2020-04-09 21:21:41 +0800

    delete: delete old version

--- README.md
+++
@@ -1,1 +0,0 @@
-# eHealingPaws-FrontEnd
--- src/services/register.js
+++
@@ -1,9 +0,0 @@
-import request from "../utils/request";
-
-export async function accountRegister(value) {
-  console.log("register service: ", value);
-  return request("/user/register", {
-    method: "POST",
-    body: value,
-  });
-}
